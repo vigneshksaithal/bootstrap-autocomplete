7739ab4f9d24d3b0df72bc7ac40b6bc525ff3410
vigneshksaithal <60360863+vigneshksaithal@users.noreply.github.com>
2020-10-28 12:32:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,91 +1,90 @@
 function autocomplete(inp, arr) {
-  var d = document.getElementById('autocomplete');
-  d.classList.add('list-group', 'position-relative', 'd-print-inline-block');
-  var currentFocus;
+  let d = document.getElementById('autocomplete')
+  d.classList.add('list-group', 'position-relative')
+  let currentFocus
   inp.addEventListener('input', function(e) {
-    var a, b, i, val = this.value;
+    let a, b, i, val = this.value
           
-    closeAllLists();
+    closeAllLists()
     if (!val) { 
-      return false;
+      return false
     }
-    currentFocus = -1; 
+    currentFocus = -1
            
-    a = document.createElement('DIV');
-    a.setAttribute('class', 'autocomplete-items');
-    a.classList.add('position-absolute', 'mt-2', 'w-100', 'rounded', 'shadow-sm'); 
-    a.style.top = '100%'; 
-    a.style.zIndex = 99; 
+    a = document.createElement('div')
+    a.classList.add('autocomplete-items', 'position-absolute', 'mt-2', 'w-100', 'rounded', 'shadow')
+    a.style.top = '100%' 
+    a.style.zIndex = 99 
       
-    this.parentNode.appendChild(a);
+    this.parentNode.appendChild(a)
     for (i = 0; i < arr.length; i++) {
       if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-        b = document.createElement('LI');
-        b.classList.add('list-group-item');
-        b.innerHTML = '<strong>' + arr[i].substr(0, val.length) + '</strong>';
-        b.innerHTML += arr[i].substr(val.length);
-        b.innerHTML += '<input type="hidden" value="' + arr[i] + '">';
+        b = document.createElement('li')
+        b.classList.add('list-group-item', 'border-bottom-0')
+        b.innerHTML = arr[i].substr(0, val.length)
+        b.innerHTML += '<strong>' + arr[i].substr(val.length) + '</strong>'
+        b.innerHTML += '<input type="hidden" value="' + arr[i] + '">'
         b.addEventListener('click', function(e) {
-          inp.value = this.getElementsByTagName('input')[0].value;
-          closeAllLists();
-        });
-        a.appendChild(b);
+          inp.value = this.getElementsByTagName('input')[0].value
+          closeAllLists()
+        })
+        a.appendChild(b)
       }
     }
-  });
+  })
   inp.addEventListener('keydown', function(e) {
-    var x = document.getElementById(this.id);
+    var x = document.getElementById(this.id)
     if (x) {
-      x = x.getElementsByTagName('div');
+      x = x.getElementsByTagName('div')
     }
     if (e.keyCode == 40) {
-      currentFocus++;
-      addActive(x);
+      currentFocus++
+      addActive(x)
     } 
     else if (e.keyCode == 38) { 
-      currentFocus--;
-      addActive(x);
+      currentFocus--
+      addActive(x)
     } 
     else if (e.keyCode == 13) {
-      e.preventDefault();
+      e.preventDefault()
       if (currentFocus > -1) {
         if (x) {
-          x[currentFocus].click();
+          x[currentFocus].click()
         }
       }
     }
-  });
+  })
     
   function addActive(x) {
     if (!x) {
-      return false;
+      return false
     }
-    removeActive(x);
+    removeActive(x)
     if (currentFocus >= x.length) {
-      currentFocus = 0;
+      currentFocus = 0
     }
     if (currentFocus < 0) {
-      currentFocus = (x.length - 1);
+      currentFocus = (x.length - 1)
     }
-    x[currentFocus].classList.add('active');
+    x[currentFocus].classList.add('active')
   }
   
   function removeActive(x) {
     for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove('active');
+      x[i].classList.remove('active')
     }
   }
   
   function closeAllLists(el) {
-    var x = document.getElementsByClassName('autocomplete-items');
+    var x = document.getElementsByClassName('autocomplete-items')
     for (var i = 0; i < x.length; i++) {
       if (el != x[i] && el != inp) {
-        x[i].parentNode.removeChild(x[i]);
+        x[i].parentNode.removeChild(x[i])
       }
     }
   }
     
   document.addEventListener('click', function (e) {
-    closeAllLists(e.target);
-  });
-}   
+    closeAllLists(e.target)
+  })
+}    
